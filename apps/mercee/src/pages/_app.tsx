8eaee9ea3bfe7ab0de599fe8e6ab3bc914fa44da
php1301 <40063990+php1301@users.noreply.github.com>
2021-02-04 07:54:43 +0700

    Update _app.tsx

--- apps/mercee/src/pages/_app.tsx
+++ apps/mercee/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { ReactComponent as NxLogo } from '../public/nx-logo-white.svg';
+import { ReactComponent as NxLogo } from '@public/nx-logo-white.svg';
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
